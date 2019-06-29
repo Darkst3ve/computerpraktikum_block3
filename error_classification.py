8c1d203fc66b6e2fff9ada52016bfca6cda15860
AdrianSchoch <50868780+AdrianSchoch@users.noreply.github.com>
2019-06-29 13:02:24 +0200

    Funktion zur Berechnung der Fehlerklassifikation

---
+++ error_classification.py
@@ -0,0 +1,17 @@
+import numpy as np
+import csv
+from file_functionD import functionD
+#functionD ist die Funktion die Marcus programmiert, das kann man gegebenenfalls noch umbenennnen
+def error_classification(A):  
+    size= np.shape(A)   
+    m=size[0] #Zeilenanzahl
+    C=[]
+    for i in range(0,m): #für jede Zeile der Daten
+        if A[i,0]== functionD(A[i,1:3]): 
+            c= 0 
+        else:
+            c= 1 
+        #dieser Abschnitt dient zur berechnung der charakteristischen Funktion
+    C.append(c)
+    result = (1/m)*(np.sum(C)) #Berechnen der Funktion
+    return print(result)
