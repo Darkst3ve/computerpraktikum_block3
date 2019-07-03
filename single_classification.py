a5a0807093a1d7c10fc6c7f17ae9d9f1144711eb
AdrianSchoch <50868780+AdrianSchoch@users.noreply.github.com>
2019-07-03 13:45:18 +0200

    Aktualisierung und Korrektur

--- single_classification.py
+++ single_classification.py
@@ -1,4 +1,5 @@
 import numpy as np
 
 def single_classification(I, D):
-    return np.sign(np.sum([D[i] for i in I]))
+    return np.sign(np.sum([D[i,0] for i in I])) #davor D[i], das war aber glaube ich falsch,da wir ja nur über die 
+    #y_i aufsummieren wollen und nicht über die x-Werte
