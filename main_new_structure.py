a5a730f85cb8d6ebcb4de7ebc9530ed5912d84de
AdrianSchoch <50868780+AdrianSchoch@users.noreply.github.com>
2019-07-08 13:49:56 +0200

    es funktioniert

--- main_new_structure.py
+++ main_new_structure.py
@@ -1,6 +1,8 @@
 import numpy as np
 from file_import import file_import
 from nearest_points import nearest_points_naive_sup
+from nearest_points import nearest_points_naive_l1
+from single_classification import single_classification
 def classify(nametest,nametrain, Kset, l):
     test = file_import(nametest)
     train= file_import(nametrain)
@@ -8,34 +10,50 @@ def classify(nametest,nametrain, Kset, l):
     D_train= train[:,1:]
     n=len(train)
     großk = int(max(Kset))
+    index_array=np.zeros((n,großk), dtype=int)
+    m_i=0
     for i in range(l):
-            train_i= train[(i*n)//l:((i+1)*n)//l,:]
-            a_i= train[0:(i*n)//l,:]
-            b_i= train[(i+1)*n//l:,:]
+            block_size = n//l
+            train_i= train[i*block_size:(i+1)*block_size,:]
+            a_i= train[0:i*block_size,:]
+            b_i= train[(i+1)*block_size:,:]
             train_strich_i = np.vstack((a_i,b_i))
             D_train_i = train_i[:,1:] #erste Spalte nicht dran
-            c_i= D_train[0:(i*n)//l,:]
-            d_i= D_train[(i+1)*n//l:,:]
+            c_i= D_train[0:i*block_size,:]
+            d_i= D_train[(i+1)*block_size:,:]
             D_train_strich_i = np.vstack((c_i,d_i))
-            m_i= len(D_train_i)
-            for j in range(0,m_i):
-                    index_j_i= nearest_points_naive_sup(D_train_strich_i[j,:],D_train_strich_i,großk)
+            index_array_i=np.zeros((m_i,großk),dtype=int)
+            for j in range(0,len(D_train_i)):
+                index_j_i= nearest_points_naive_sup(D_train_i[j,:],D_train_strich_i,großk)
+                index_array[m_i+j,:]=index_j_i
+            m_i= m_i + len(D_train_i)
     list_ks = []
     for k in Kset:
         errorarray=[]
         for i in range(l):
             m_i= len(D_train_i)
+            block_size = n//l
+            train_i= train[i*block_size:(i+1)*block_size,:]
+            a_i= train[0:i*block_size,:]
+            b_i= train[(i+1)*block_size:,:]
+            train_strich_i = np.vstack((a_i,b_i))
+            D_train_i = train_i[:,1:] #erste Spalte nicht dran
+            c_i= D_train[0:i*block_size,:]
+            d_i= D_train[(i+1)*block_size:,:]
+            D_train_strich_i = np.vstack((c_i,d_i))
+            index_array_i=np.zeros((m_i,großk),dtype=int)
             C_i=[]
-           
             for j in range(0,m_i):
-                if train[j,0] == np.sign(np.sum(train_strich_i[index_j_i[:int(k)],0])):
+                if train_i[j,0] == np.sign(np.sum(train_strich_i[index_array[m_i + j,:int(k)],0])):
                     c=0
                 else:
                     c=1
                 C_i.append(c)
+            m_i += len(D_train_i)
             error_classification_i= 1/m_i *sum(C_i)
             errorarray.append(error_classification_i)
         middle_k= (1/l)*sum(errorarray)
         list_ks.append(middle_k)
     print(list_ks)
-    print(list_ks.index(min(list_ks))
+    print([np.abs(list_ks[i] - list_ks[i + 1]) for i in range(len(list_ks) - 1)])
+    print(list_ks.index(min(list_ks)))
