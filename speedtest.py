1754c52c82b3c1ba44aea6ee0fce243469d8288f
Lennart <lennart.duvenbeck@gmail.com>
2019-07-20 14:10:40 +0200

    Minimale Änderung

--- speedtest.py
+++ speedtest.py
@@ -5,42 +5,43 @@ from nearest_points import nearest_points_naive_l1
 from nearest_points import nearest_points_naive_sup
 from scipy.spatial import KDTree
 
-file_name = "toy-10d.train.csv"
+file_name = "ijcnn1.train.csv"
+k = 200
 
 data = file_import(file_name)
 n = data.shape[0]
-result_array_1 = np.zeros((n, 200))
+result_array_1 = np.zeros((n, k))
 tic = time.time()
 for i in range(n):
-    result_array_1[i, :] = nearest_points_naive_l1(data[i, :], data, 200)
+    result_array_1[i, :] = nearest_points_naive_l1(data[i, :], data, k)
 toc = time.time()
 print("l1-naiv : %.10f seconds" % (toc - tic))
 
 data = file_import(file_name)
 n = data.shape[0]
-result_array_2 = np.zeros((n, 200))
+result_array_2 = np.zeros((n, k))
 tic = time.time()
 for i in range(n):
-    result_array_2[i, :] = nearest_points_naive_sup(data[i, :], data, 200)
+    result_array_2[i, :] = nearest_points_naive_sup(data[i, :], data, k)
 toc = time.time()
 print("lsup-naiv : %.10f seconds" % (toc - tic))
 
 data = file_import(file_name)
 n = data.shape[0]
-result_array_3 = np.zeros((n, 200))
+result_array_3 = np.zeros((n, k))
 tic = time.time()
 kdt = KDTree(data[:, 1:])
 for i in range(n):
-    result_array_3[i, :] = kdt.query(data[i, 1:], k=200, p=1)[1]
+    result_array_3[i, :] = kdt.query(data[i, 1:], k=k, p=1)[1]
 toc = time.time()
 print("l1-kdt : %.10f seconds" % (toc - tic))
 
 data = file_import(file_name)
 n = data.shape[0]
-result_array_4 = np.zeros((n, 200))
+result_array_4 = np.zeros((n, k))
 tic = time.time()
 kdt = KDTree(data[:, 1:])
 for i in range(n):
-    result_array_4[i, :] = kdt.query(data[i, 1:], k=200, p=float("inf"))[1]
+    result_array_4[i, :] = kdt.query(data[i, 1:], k=k, p=float("inf"))[1]
 toc = time.time()
 print("lsup-kdt : %.10f seconds" % (toc - tic))
